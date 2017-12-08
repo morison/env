e27ff2ee88a2fcd8563b18f5e02c6a4be5acbabc
Naruhito.MORI <mori@local-pc.local>
2017-12-08 18:34:55 +0900

    add balias config

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+# alias for git
+balias gc 'git checkout'
+balias gct 'git commit'
+balias gf 'git fetch'
+balias gs 'git status'
+balias gd 'git diff'
+balias gpll 'git pull'
+balias gpsh 'git push'
+balias ga 'git add'
+
+# general
+balias ll 'ls -al'
